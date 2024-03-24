0d9cab18167c94a7d9cf4bcf7ecc2c7a3a7abedd
Geovanny Bucay <ebucayle@pichincha.com>
2024-03-24 18:46:04 -0500

    add PORT

--- src/main.ts
+++ src/main.ts
@@ -18,6 +18,6 @@ async function bootstrap() {
   app.use(bodyParser.json({ limit: '10mb' }));
   app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
